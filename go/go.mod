7f9c13166073c29f51f304376b21aea8b643aa56
uppercase <uppercasee@proton.me>
2025-05-21 08:55:02 +0545

    Added test for handler

--- go/go.mod
+++ go/go.mod
@@ -6,11 +6,15 @@ require google.golang.org/grpc v1.72.1
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
 	github.com/redis/go-redis/v9 v9.8.0
+	github.com/stretchr/testify v1.10.0
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
---
+++ go/internal/handler/grpc_handler_test.go
@@ -0,0 +1,47 @@
+package handler_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/uppercasee/drls/internal/handler"
+	"github.com/uppercasee/drls/internal/redis"
+	"github.com/uppercasee/drls/pb"
+)
+
+func TestHandleCheck(t *testing.T) {
+	ctx := context.Background()
+	clientID := "test-client-123"
+	key := "rate_limit:" + clientID
+
+	redis_server.InitRedis()
+	redis_server.RDB.Del(ctx, key)
+
+	h := handler.NewGRPCHandler()
+
+	// Send requests within the limit (5)
+	for range 5 {
+		resp, err := h.HandleCheck(ctx, &pb.CheckRequest{ClientId: clientID})
+		assert.NoError(t, err)
+		assert.True(t, resp.Allowed)
+		assert.Equal(t, int64(0), resp.RetryAfter)
+	}
+
+	// This 6th request should be rejected
+	resp, err := h.HandleCheck(ctx, &pb.CheckRequest{ClientId: clientID})
+	assert.NoError(t, err)
+	assert.False(t, resp.Allowed)
+	assert.Greater(t, resp.RetryAfter, int64(0))
+
+	// rate limit window expires
+	time.Sleep(time.Duration(resp.RetryAfter+1) * time.Second)
+
+	resp, err = h.HandleCheck(ctx, &pb.CheckRequest{ClientId: clientID})
+	assert.NoError(t, err)
+	assert.True(t, resp.Allowed)
+
+	// Cleanup
+	redis_server.RDB.Del(ctx, key)
+}
